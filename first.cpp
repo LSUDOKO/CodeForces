74ebd0d0e6070059436039cbdcf3de81080fc34a
LSUDOKO <adoranto737@gmail.com>
2025-06-14 00:48:41 +0530

    solve fisrt cpp problem

---
+++ first.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int a;
+    cin>>a; 
+    while(a>0){
+        int n;
+        cin>>n;
+        cout<<n-1<<endl;
+        a--;
+    }
+    return 0;
+}
