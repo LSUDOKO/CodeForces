ca8733e490de42397f337602897cf6f5277f5a57
LSUDOKO <adoranto737@gmail.com>
2025-07-08 23:53:55 +0530

    add problem

--- Rating 800/A. Serval and Mocha's Array.cpp
+++ Rating 800/A. Serval and Mocha's Array.cpp
@@ -1,15 +1,43 @@
-#include<iostream>
+#include <iostream>
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-    int t;
-    cin>>t;
-    while(t--){
-        int n;
-        cin>>n;
-        vector<int> a(n);
-        for(int i=0; i<n; i++){
-            cin>>a[i];
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+bool isPossible(const vector<int>& a) {
+    int n = a.size();
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (gcd(a[i], a[j]) <= 2) {
+                return true;
+            }
         }
     }
+    return false;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+        }
+        if (isPossible(a)) {
+            cout << "Yes" << endl;
+        } else {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
 }
