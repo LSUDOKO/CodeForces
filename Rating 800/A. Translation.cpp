d26850997849cd01a6f0954764a9c4d54d1f5c33
LSUDOKO <adoranto737@gmail.com>
2025-08-14 22:32:46 +0530

    translation

---
+++ Rating 800/A. Translation.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    string t;
+    cin>>t;
+    string a="";
+    for (int i=s.size()-1; i>=0; i--){
+        a+=s[i];
+    }
+    if (a==t){
+        cout<<"YES"<<endl;
+    } else {
+        cout<<"NO"<<endl;
+    }
+}
