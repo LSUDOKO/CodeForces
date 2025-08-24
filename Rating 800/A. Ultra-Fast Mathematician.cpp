4e584c75dc7598c4f7454e5fed39833bdca52536
LSUDOKO <adoranto737@gmail.com>
2025-08-25 00:32:34 +0530

    ultra-fast

---
+++ Rating 800/A. Ultra-Fast Mathematician.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s1, s2;
+    cin >> s1 >> s2;
+    string s3 = "";
+    for (int i = 0; i < s1.size(); i++) {
+        if (s1[i] == s2[i]) 
+            s3.push_back('0');
+        else 
+            s3.push_back('1');  
+    }
+    cout << s3;
+}
