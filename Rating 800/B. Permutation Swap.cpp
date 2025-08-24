2fa66372b99690a77a89ffc6042bccb347edd264
LSUDOKO <adoranto737@gmail.com>
2025-08-24 23:53:53 +0530

    permutation swap

---
+++ Rating 800/B. Permutation Swap.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr(n+1);
+        for (int i=1;i<=n;i++){
+            cin>>arr[i];
+        }
+        int gc=0;
+        for (int i=1;i<=n;i++){
+            gc=gcd(gc,abs(arr[i]-i));
+        }
+        cout<<gc<<endl;
+
+
+    }
+}
