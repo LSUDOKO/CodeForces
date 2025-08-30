8aa9559c00f759f96d4d943f45fb4e74160cd516
LSUDOKO <adoranto737@gmail.com>
2025-08-30 23:59:00 +0530

    hit lottery

--- Rating 800/A. Hit the Lottery.cpp
+++ Rating 800/A. Hit the Lottery.cpp
@@ -1,3 +1,21 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     long long n;
+//     cin >> n;
+//     int count = 0;
+//     int bills[] = {100, 20, 10, 5, 1};
+
+//     for (int i = 0; i < 5; i++) {
+//         count += n / bills[i];
+//         n %= bills[i];
+//     }
+
+//     cout << count << endl;
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -5,13 +23,30 @@ int main() {
     long long n;
     cin >> n;
     int count = 0;
-    int bills[] = {100, 20, 10, 5, 1};
 
-    for (int i = 0; i < 5; i++) {
-        count += n / bills[i];
-        n %= bills[i];
+    while (n > 0) {
+        if (n >= 100) {
+            count += n / 100;
+            n %= 100;
+        } 
+        else if (n >= 20) {
+            count += n / 20;
+            n %= 20;
+        } 
+        else if (n >= 10) {
+            count += n / 10;
+            n %= 10;
+        } 
+        else if (n >= 5) {
+            count += n / 5;
+            n %= 5;
+        } 
+        else {
+            count += n;   // only 1-dollar bills remain
+            n = 0;
+        }
     }
 
-    cout << count << endl;
+    cout << count << "\n";
     return 0;
 }
