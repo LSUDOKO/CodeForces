432517aca5eff8936fc44fa361679fa19f2546d5
LSUDOKO <adoranto737@gmail.com>
2025-09-04 14:27:06 +0530

    add array cloning technique

---
+++ Rating 900/B. Array Cloning Technique.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        map<int, int> freq;
+
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            freq[arr[i]]++;
+        }
+
+        int countmaxi = 0;
+        for (auto &p : freq) {
+            countmaxi = max(countmaxi, p.second);
+        }
+
+        int operations = 0;
+        int curr = countmaxi;
+
+        while (curr < n) {
+            operations++;                 // one copy operation
+            int can_fill = min(curr, n - curr);
+            curr += can_fill;             // fill new elements
+        }
+
+        cout << operations + (n - countmaxi) << "\n";
+    }
+}
