2a58980481ecb1c986a23624cc3db582c608bc9d
LSUDOKO <adoranto737@gmail.com>
2025-09-09 02:21:09 +0530

    add strnge array

--- Rating 900/A. Strange array.cpp
+++ Rating 900/A. Strange array.cpp
@@ -1,22 +1,26 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
+
+int main() {
     int t;
-    cin>>t;
-    while(t--){
-        int n,x;
-        cin>>n>>x;
-        vector<int>arr(n);
-        for (int i=0;i<n;i++){
-            cin>>arr[i];
+    cin >> t;
+    while (t--) {
+        long long n, x;
+        cin >> n >> x;
+        vector<long long> arr(n);
+        for (long long i = 0; i < n; i++) {
+            cin >> arr[i];
         }
-        int summin=0;
-        int summax=0;
-        for(int i=0;i<n;i++){
-            summax+=ceil(arr[i]/(float)x);
-            summin+=arr[i];
+
+        long long summin = 0;
+        long long summax = 0;
+
+        for (int i = 0; i < n; i++) {
+            summax += ceil((double)arr[i] / x);
+            summin += arr[i];
         }
-        int summinresult=ceil(summin/n);
-        cout<<summinresult<<" "<<summax<<endl;
+
+        long long summinresult = ceil((double)summin / x);
+        cout << summinresult << " " << summax << endl;
     }
 }
