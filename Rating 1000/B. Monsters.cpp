dcddb06a938fc1d55ee704b9219cf0730c68f788
LSUDOKO <adoranto737@gmail.com>
2025-09-13 03:07:28 +0530

    add monster

---
+++ Rating 1000/B. Monsters.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n; long long k;
+        cin >> n >> k;
+        vector<pair<long long,int>> v;
+        for (int i = 0; i < n; i++) {
+            long long x; cin >> x;
+            long long r = x % k;
+            if (r == 0) r = k;
+            v.push_back({-r, i+1}); 
+        }
+        sort(v.begin(), v.end());
+        for (auto &p : v) cout << p.second << " ";
+        cout <<endl;
+    }
+    return 0;
+}
