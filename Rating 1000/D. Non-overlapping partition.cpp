7023c1ab97f1c24ee85e0847a10779fa6e4c3e26
LSUDOKO <adoranto737@gmail.com>
2025-09-16 19:16:03 +0530

    add 1000

--- Rating 1000/D. Non-overlapping partition.cpp
+++ Rating 1000/D. Non-overlapping partition.cpp
@@ -1,7 +1,46 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+//         string s;
+//         cin >> s;
+
+//         set<char> s1;
+//         set<char> s2;
+//         int status = n; 
+
+//         for (int i = 0; i < n; i++) {
+//             if (s1.find(s[i]) == s1.end()) {
+//                 s1.insert(s[i]);
+//             } else {
+//                 status = i; 
+//                 break;
+//             }
+//         }
+
+//         for (int i = status; i < n; i++) {
+//             if (s2.find(s[i]) == s2.end()) {
+//                 s2.insert(s[i]);
+//             // s2.insert(s[i]);
+
+//         }}
+
+//         cout << s1.size() + s2.size() << endl;
+//     }
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--) {
@@ -10,27 +49,33 @@ int main() {
         string s;
         cin >> s;
 
-        set<char> s1;
-        set<char> s2;
-        int status = n; 
+        vector<int> prefix_unique(n);
+        vector<int> suffix_unique(n);
 
-        for (int i = 0; i < n; i++) {
-            if (s1.find(s[i]) == s1.end()) {
-                s1.insert(s[i]);
-            } else {
-                status = i; 
-                break;
-            }
+        // Calculate prefix_unique
+        set<char> current_unique_prefix;
+        for (int i = 0; i < n; ++i) {
+            current_unique_prefix.insert(s[i]);
+            prefix_unique[i] = current_unique_prefix.size();
         }
 
-        for (int i = status; i < n; i++) {
-            if (s2.find(s[i]) == s2.end()) {
-                s2.insert(s[i]);
-            // s2.insert(s[i]);
+        // Calculate suffix_unique
+        set<char> current_unique_suffix;
+        for (int i = n - 1; i >= 0; --i) {
+            current_unique_suffix.insert(s[i]);
+            suffix_unique[i] = current_unique_suffix.size();
+        }
 
-        }}
-
-        cout << s1.size() + s2.size() << endl;
+        int max_sum = 0;
+        // Iterate through all possible split points
+        // A split at index 'i' means a = s[0...i] and b = s[i+1...n-1]
+        // 'a' will be prefix_unique[i]
+        // 'b' will be suffix_unique[i+1]
+        // The last valid split point for 'i' is n-2 (to ensure 'b' is non-empty)
+        for (int i = 0; i < n - 1; ++i) {
+            max_sum = max(max_sum, prefix_unique[i] + suffix_unique[i + 1]);
+        }
+        cout << max_sum << endl;
     }
     return 0;
 }
