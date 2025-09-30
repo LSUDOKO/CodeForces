61acfffa75d76ecbb0bf01e702bef01910a55165
LSUDOKO <adoranto737@gmail.com>
2025-09-30 23:48:08 +0530

    add solution for C. Double-sided lines problem

---
+++ Rating 1000/C. Double-sided lines.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int t;
+	cin >> t; 
+	while (t--)
+	{
+		string A, B;
+		cin >> A >> B; 
+
+		long long n = A.size(), m = B.size();
+		long long lcs = 0; 
+
+		for (long long len = 1; len <= min(n, m); len++)
+		{
+
+			for (long long i = 0; i + len <= n; i++)
+			{
+
+				for (long long j = 0; j + len <= m; j++)
+				{
+					
+					string extract_A = A.substr(i, len);
+					string extract_B = B.substr(j, len);
+
+					
+					if (extract_A == extract_B)
+						lcs = max(lcs, len); 
+				}
+			}
+		}
+
+		long long operations = n + m - 2 * lcs;
+		cout << operations << endl; 
+	}
+	return 0;
+}
+
+// Time Complexity (TC): O(20^4) = O(160000) ~ O(10^5)
+// Space Complexity (SC): O(2*20)
