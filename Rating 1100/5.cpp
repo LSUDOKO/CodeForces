2c5f6bbee7104f944d756c0a9f60d3f7a32b7050
LSUDOKO <adoranto737@gmail.com>
2025-11-05 18:56:40 +0530

    add 1100

---
+++ Rating 1100/5.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>ans(n);
+        for (int i=0;i<n;i++){
+            cin>>ans[i];
+        }
+        vector<int>div;
+        for (int i=0;i<sqrt(n);i++){
+            if
+        }
+    }
+}
