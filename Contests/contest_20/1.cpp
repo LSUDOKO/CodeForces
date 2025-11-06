5bc930e6f957aca01fb723522e5d987e67252a61
LSUDOKO <adoranto737@gmail.com>
2025-11-06 23:01:18 +0530

    add contest20

---
+++ Contests/contest_20/1.cpp
@@ -0,0 +1,69 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+
+//     int t;
+//     cin>>t;
+//     while(t--){
+//         int n;
+//         cin>>n;
+
+//         vector<int>ans(n);
+//         for (int i=0;i<n;i++){
+//             cin>>ans[i];
+//         }
+//         int x;
+//         cin>>x;
+//         bool check=false;
+//         for (int i=0;i<n-1;i++){
+//             if ((x>=ans[i] && x<=ans[i+1] )|| (x<=ans[i] && x>=ans[i+1])){
+//                 check=true;
+//                 break;
+//             }
+//         }
+//         if (check){
+//             cout<<"YES"<<endl;
+//         }
+//         else {
+//             cout<<"NO"<<endl;
+//         }
+//     }
+    
+
+// }
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+
+        vector<int>ans(n);
+        for (int i=0;i<n;i++){
+            cin>>ans[i];
+        }
+        int x;
+        cin>>x;
+        int maxi=INT_MIN;
+        int mini=INT_MAX;   
+        for (int i=0;i<n;i++){
+            maxi=max(maxi,ans[i]);
+            mini=min(mini,ans[i]);
+        }
+        if (x>=mini && x<=maxi ){
+            cout<<"YES"<<endl;
+            
+            }
+        else{
+            cout<<"NO"<<endl;
+        }
+    
+
+}
+
+
--- Rating 1100/4.cpp
+++ Rating 1100/4.cpp
@@ -1,33 +1,76 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int n;
+//         cin >> n;
+//         vector<long long> a(n);
+//         for (int i = 0; i < n; i++) cin >> a[i];
+
+//         long long ans = LLONG_MIN;
+//         long long cur = a[0];
+
+//         for (int i = 1; i < n; i++) {
+//             if ((a[i] % 2 + 2) % 2 != (a[i - 1] % 2 + 2) % 2){
+
+
+//                 cur = max(a[i], cur + a[i]);}
+//             else{
+//                 cur = a[i];
+//             }
+
+//             ans = max(ans, cur);
+//         }
+
+//         ans = max(ans, cur);
+//         cout << ans << endl;
+//     }
+
+// }
+
+
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 
 int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
     int t;
     cin >> t;
+
     while (t--) {
-        int n;
+        ll n;
         cin >> n;
-        vector<long long> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
+        vector<ll> v(n);
+        for (int i = 0; i < n; i++) cin >> v[i];
+        ll ans = LLONG_MIN, sum = 0;
+        int i = 0, j = 0;
 
-        long long ans = LLONG_MIN;
-        long long cur = a[0];
-
-        for (int i = 1; i < n; i++) {
-            if ((a[i] % 2 + 2) % 2 != (a[i - 1] % 2 + 2) % 2){
-
-
-                cur = max(a[i], cur + a[i]);}
-            else{
-                cur = a[i];
+        while (j < n) {
+            if (sum < 0) {
+                sum = 0;
+                i = j;
             }
 
-            ans = max(ans, cur);
+            if (i < j) {
+                if ((v[j] ^ v[j - 1]) & 1)
+                    sum += v[j];
+                else {
+                    sum = v[j];
+                    i = j;
+                }
+            } else sum = v[j];
+
+            ans = max(ans, sum);
+            j++;
         }
 
-        ans = max(ans, cur);
-        cout << ans << endl;;
+        cout << ans << "\n";
     }
-
 }
