675b2241477058db1dd48ee3c0b3ff06309527a9
LSUDOKO <adoranto737@gmail.com>
2025-11-17 17:55:35 +0530

    add 1100

---
+++ Rating 1100/C. Make it Equal.cpp
@@ -0,0 +1,30 @@
+#include<bitss/stdc++.h>
+using namespace std;
+ int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n,k;
+        cin>>n>>k;
+        vector<int>s(n);
+        vector<int>t(n);
+        map<int,int>sc;
+        map<int,int>tc;
+        
+        for (int i=0;i<n;i++){
+            cin>>s[i];
+        }
+        for (int i=0;i<n;i++){
+            cin>>t[i];
+        }
+        for (int i=0;i<n;i++){
+            sc[s[i]]++;
+        }
+        for (int i=0;i<n;i++){
+            tc[t[i]]++;
+        }
+        for (int i=0;i<)
+
+
+    }
+ }
