6d986c10ada5b585b87c31b69dd381319744eed9
LSUDOKO <adoranto737@gmail.com>
2026-01-11 19:09:10 +0530

    add 1300

---
+++ Rating 1300/B. Lucky Numbers (easy).cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<long long>luck;
+void generate(long long curr,int count4,int count7){
+    if (count4+count7>10){
+        return ;
+    }
+    if (count4>0 && count4==count7){
+        luck.push_back(curr);
+    }
+    generate(curr*10+4,count4+1,count7);
+    generate(curr*10+7,count4,count7+1);
+
+}
+int main()   {
+    long long  n;
+    cin>>n
+    generate(0,0,0);
+    sort(luck.begin(),luck.end());
+    for (long long val:luck){
+        if (val>=n){
+            cout<<val<<endl;
+            return 0;
+        }
+    }
+    return 0;
+}
