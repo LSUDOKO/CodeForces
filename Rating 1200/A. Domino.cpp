475dd77433e55c880aedf318962ea39a1a2dfe9c
LSUDOKO <adoranto737@gmail.com>
2026-02-05 19:12:19 +0530

    add 1200

---
+++ Rating 1200/A. Domino.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int sumx=0;
+    int sumy=0;
+    bool flip=false;
+    while(n--){
+        int x,y;
+        cin>>x>>y;
+        sumx+=x;
+        sumy+=y;
+        if (x%2!=y%2){
+            flip=true;
+        }
+        
+    }
+    if (sumx%2==0 && sumy%2==0){
+        cout<<0<<endl;
+    }
+    else if (sumx%2!=0 && sumy%2!=0 && flip){
+        cout<<1<<endl;
+    }
+    else {
+        cout<<-1<<endl;
+    }
+
+}
